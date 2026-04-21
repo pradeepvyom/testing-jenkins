14b432ff64c3fe497d28b153c4ee3d42b0e1cfc5
pradeepvyom <pradeep@vyom.ai>
2026-04-21 11:34:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello from Jenkins Pradeep CI/CD is ready! 🚀</h1><p>Version 2.0</p>');
+    res.end('<h1>Hello from Jenkins Pradeep CI/CD! 🚀</h1><p>Version 2.0</p>');
 });
 
 server.listen(3000, () => {
